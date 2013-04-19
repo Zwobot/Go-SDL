5b16d3bac5115b5ffab21a9236bb66906a656fd6
Zwobot <kblindert@trillian.trillian>
2013-04-19 15:38:41 +0200

    Added in RGB loss compensation table from SDL_pixels.c. Added Pixel32() method to Surface to aid writing performant code. Commented on MapRGBA/GetRGBA that the functions shouldn't be used in inner loops and provided example code.

--- sdl/sdl.go
+++ sdl/sdl.go
@@ -21,11 +21,11 @@ import "C"
 
 import (
 	"os"
-	"reflect"
 	"runtime"
 	"sync"
 	"time"
 	"unsafe"
+	"reflect"
 )
 
 type cast unsafe.Pointer
@@ -534,14 +534,76 @@ func (s *Surface) SetClipRect(r *Rect) {
 	s.mutex.Unlock()
 }
 
+// ==================
+// Pixel Manipulation
+// ==================
+
+var ExpandByte [9][]uint32 = [9][]uint32 {
+	{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255},
+	{ 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 255},
+	{ 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 85, 89, 93, 97, 101, 105, 109, 113, 117, 121, 125, 129, 133, 137, 141, 145, 149, 153, 157, 161, 165, 170, 174, 178, 182, 186, 190, 194, 198, 202, 206, 210, 214, 218, 222, 226, 230, 234, 238, 242, 246, 250, 255},
+	{ 0, 8, 16, 24, 32, 41, 49, 57, 65, 74, 82, 90, 98, 106, 115, 123, 131, 139, 148, 156, 164, 172, 180, 189, 197, 205, 213, 222, 230, 238, 246, 255},
+	{ 0, 17, 34, 51, 68, 85, 102, 119, 136, 153, 170, 187, 204, 221, 238, 255},
+	{ 0, 36, 72, 109, 145, 182, 218, 255},
+	{ 0, 85, 170, 255 },
+	{ 0, 255 },
+	{ 255 },
+}
+
 // Map a RGBA color value to a pixel format.
+//
+// You can do the pixel mapping in inner loops with the
+// code shown in the function below. (Except palette handling)
+// 
+// func MapRGBA(format *PixelFormat, r, g, b, a uint8) uint32 {
+// 	if format.Palette == nil {
+// 		return uint32(r>>format.Rloss)<<format.Rshift |
+// 			uint32(g>>format.Gloss)<<format.Gshift |
+// 			uint32(b>>format.Bloss)<<format.Bshift |
+// 			uint32(a>>format.Aloss)<<format.Ashift&format.Amask
+// 	}
+// 	return sdl.MapRGBA(format, r,g,b,a)
+// }
 func MapRGBA(format *PixelFormat, r, g, b, a uint8) uint32 {
 	return (uint32)(C.SDL_MapRGBA((*C.SDL_PixelFormat)(cast(format)), (C.Uint8)(r), (C.Uint8)(g), (C.Uint8)(b), (C.Uint8)(a)))
 }
 
-// Gets RGBA values from a pixel in the specified pixel format.
+func MapRGB(format *PixelFormat, r, g, b uint8) uint32 {
+	return (uint32)(C.SDL_MapRGB((*C.SDL_PixelFormat)(cast(format)), (C.Uint8)(r), (C.Uint8)(g), (C.Uint8)(b)))
+}
+
+// Functionally the inverse of MapRGBA. The same performance considerations apply.
+//
+// func GetRGBA(color uint32, format *PixelFormat, r, g, b, a *uint8) {
+//     if (format.Palette == nil) {
+//         var v uint32
+//         v = (color & format.Rmask) >> format.Rshift
+//         *r = sdl.ExpandByte[format.Rloss][v]
+//         v = (color & format.Gmask) >> format.Gshift
+//         *g = sdl.ExpandByte[format.Gloss][v]
+//         v = (color & format.Bmask) >> format.Bshift
+//         *b = sdl.ExpandByte[format.Bloss][v]
+//         v = (color & format.Amask) >> format.Ashift
+//         *a = sdl.ExpandByte[format.Aloss][v]
+//     } else {
+//     	sdl.GetRGBA(color, format, r, g, b, a)
+//     }
+// }
+func GetRGB(color uint32, format *PixelFormat, r, g, b *uint8) {
+	C.SDL_GetRGB(C.Uint32(color), (*C.SDL_PixelFormat)(cast(format)), (*C.Uint8)(r), (*C.Uint8)(g), (*C.Uint8)(b))
+}
+
 func GetRGBA(color uint32, format *PixelFormat, r, g, b, a *uint8) {
 	C.SDL_GetRGBA(C.Uint32(color), (*C.SDL_PixelFormat)(cast(format)), (*C.Uint8)(r), (*C.Uint8)(g), (*C.Uint8)(b), (*C.Uint8)(a))
+}
+
+// Access the pixels of a 4 byte per pixel surface as []uint32.
+//
+// BUG(Zwobot) Pixel 32 doesn't handle surfaces with an offset or pitch not aligned to uint32.
+func (s *Surface) Pixel32() []uint32 {
+	length := int(s.Pitch) * int(s.H) / 4
+	header := reflect.SliceHeader{uintptr(unsafe.Pointer(s.Pixels)), length, length}
+	return (*(*[]uint32)(unsafe.Pointer(&header)))
 }
 
 // Loads Surface from file (using IMG_Load).
